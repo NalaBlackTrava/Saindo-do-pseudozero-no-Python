1f4e585944703f47b2335077149a909b1466a86e
Nala Ayaba <joaovictor070102@gmail.com>
2022-02-20 19:33:32 -0300

    ip

--- AnaliseBonusViagem.py
+++ AnaliseBonusViagem.py
@@ -1,3 +1,11 @@
-#passo a passo de solução
+#Passo a passo de solução
 
-saa
+#Abrir os 6 arquivos em excel
+
+#Para cada arquivo:
+
+#Verificar se algum valor na coluna B daquele arquivo é maior que 55k
+
+#If X > 55k --> envia um SMS com o nome, o mês e as vendas delu
+
+#Else --> nothing 
