a5d5c89a0ee47b177c411aff218ebfb33a896748
Nala Ayaba <joaovictor070102@gmail.com>
2022-02-20 21:38:57 -0300

    wip debugado e com vdds

--- AnaliseBonusViagem.py
+++ AnaliseBonusViagem.py
@@ -5,13 +5,11 @@ import pandas as pd
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mes in lista_meses:
-    print(mes)
     tabela_vendas = pd.read_excel (f'{mes}.xlsx')
-    print (tabela_vendas)
-    If (tabela_vendas:['Vendas']> 550000).any():
-        print('Uffas, agora tu pode viajar, seu chefe financiou um novo ape na Barra')
+    if (tabela_vendas['Vendas']> 55000).any():
+        print(f'No mês {mes} teve ume trabalhador q vendeu mais de 55k, uffas, agora tu pode viajar, seu chefe financiou um novo ape na Barra')
 
 
 #If X > 55k --> envia um SMS com o nome, o mês e as vendas delu
 
-#Else --> nothing 
+#Else --> nothing
