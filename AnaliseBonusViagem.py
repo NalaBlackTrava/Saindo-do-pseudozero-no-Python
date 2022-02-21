86f1da290024ad0a80cd0f028cc2479500569312
Nala Ayaba <joaovictor070102@gmail.com>
2022-02-21 15:31:00 -0300

    uffas, terminei

--- AnaliseBonusViagem.py
+++ AnaliseBonusViagem.py
@@ -1,7 +1,11 @@
 from ast import If
 from msilib.schema import Condition
-from twilio.rest import Client
 import pandas as pd
+from twilio.rest import Client
+
+account_sid = "AC95b45026db107ed681e049d4988d3d49"
+auth_token  = "13464e8cf85fc6535e963693275f87dc"
+client = Client(account_sid, auth_token)
 
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
@@ -10,24 +14,7 @@ for mes in lista_meses:
     if (tabela_vendas['Vendas']> 55000).any():
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas']> 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas']> 55000, 'Vendas'].values[0]
-        print(f'No mês de {mes}, {vendedor} vendeu R$ {vendas},00, ou seja, bateu a meta.\n \n  Uffas, agora que o chefe financiou um novo apê na Barra, vc vai poder viajar... ☭\n  Continue assim e quem sabe ele passa uma Panamera ano que vem :)\n')
-
-
-
-# Your Account SID from twilio.com/console
-account_sid = "AC95b45026db107ed681e049d4988d3d49"
-# Your Auth Token from twilio.com/console
-auth_token  = "c1f1c9c6c6c437dfcd7eef71045679c9"
-
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    to="+559714803509", 
-    from_="+19126003150",
-    body="Hello from Python!")
-
-print(message.sid)
-
-#If X > 55k --> envia um SMS com o nome, o mês e as vendas delu
-
-#Else --> nothing
+        message = client.messages.create(
+            to="+55011971483509", 
+            from_="+19126003150",
+            body=f'No mês de {mes}, {vendedor} vendeu R$ {vendas},00, ou seja, bateu a meta.\n \n  Uffas, agora que o chefe financiou um novo apê na Barra, vc vai poder viajar... ☭\n  Continue assim e quem sabe ele passa uma Panamera ano que vem :)\n')
